305411696f926ec2f64acb4d6d72a68c7e2b5ce1
Charlie G <charlieg895@gmail.com>
2020-12-02 17:36:26 -0600

    reorg day 1

--- day01/day01.py
+++ day01/day01.py
@@ -1,6 +1,6 @@
-data = '''''' # provided input data goes here
+with open('data.txt') as f:
+    data = list({int(x.strip()) for x in f.readlines()})
 
-data = list({int(x.strip()) for x in data.split('\n')})
 
 def find_solution1(data, total):
     for i, x in enumerate(data[:-1]):
@@ -10,6 +10,7 @@ def find_solution1(data, total):
     else:
         return None
 
+
 def find_solution2(data):
     for i, n in enumerate(data[:-2]):
         add_result = find_solution1(data[i+1:], 2020-n)
@@ -17,6 +18,7 @@ def find_solution2(data):
             return n * add_result
     else:
         return None
+
 
 print(find_solution1(data, 2020))
 print(find_solution2(data))
