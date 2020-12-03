6869f87eab2a992189d4432261b2d721c7d741de
Charlie G <charlieg895@gmail.com>
2020-12-03 11:22:46 -0600

    day 3 solution

---
+++ day03/day03.py
@@ -0,0 +1,23 @@
+import math
+
+with open('data.txt') as f:
+    data = [x.rstrip() for x in f.readlines()]
+
+
+def count_trees(data, dx, dy):
+    trees = 0
+    x, y = 0, 0
+    while y < len(data):
+        if data[y][x] == '#':
+            trees += 1
+        y += dy
+        x = (x + dx) % len(data[0])
+    return trees
+
+
+# part 1
+print(count_trees(data, 3, 1))
+# part 2
+answer = math.prod(count_trees(data, dx, dy)
+                   for dx, dy in zip([1, 3, 5, 7, 1], [1]*4+[2]))
+print(answer)
