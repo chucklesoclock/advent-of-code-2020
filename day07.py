ad32f46be5bdeb04910335baffd135ca67155719
Charlie G <charlieg895@gmail.com>
2020-12-08 23:32:41 -0600

    day 7 part 2 recursive sum wondering if there are speed savings by building a list of "seen" counts

--- day07.py
+++ day07.py
@@ -1,21 +1,52 @@
 with open('input/data07.txt') as f:
     data = f.read().splitlines()
 
-d = dict()
-for line in data:
-    k, v = line.split(' contain ')
-    k = k[:-len(' bags')]
-    v = [''.join(ch for ch in x.replace('bags', 'bag').replace(
-        ' bag', '') if not ch.isnumeric()).strip() for x in v.rstrip('.').split(', ')]
-    d.update({k: v if not v[0] == 'no other' else []})
+
+def part1():
+    d = dict()
+    for line in data:
+        k, v = line.split(' contain ')
+        k = k[:-len(' bags')]
+        v = [''.join(ch for ch in x.replace('bags', 'bag').replace(
+            ' bag', '') if not ch.isnumeric()).strip() for x in v.rstrip('.').split(', ')]
+        d[k] = v if not v[0] == 'no other' else []
+
+    def can_carry_shiny(bag):
+        sub_bags = d[bag]
+        if not sub_bags:
+            return False
+        else:
+            return 'shiny gold' in sub_bags or any(can_carry_shiny(b) for b in sub_bags)
+
+    print(sum(can_carry_shiny(bag) for bag in d))
 
 
-def can_carry_shiny(bag):
-    sub_bags = d[bag]
-    if not sub_bags:
-        return False
-    else:
-        return 'shiny gold' in sub_bags or any(can_carry_shiny(b) for b in sub_bags)
+def part2():
+    d = dict()
+    for line in data:
+        k, v = line.split(' contain ')
+        k = k[:-len(' bags')]
+        v = [''.join(ch for ch in x.replace('bags', 'bag').replace(
+            ' bag', '')).strip() for x in v.rstrip('.').split(', ')]
+        if v[0] == 'no other':
+            vd = dict()
+        else:
+            vd = dict()
+            for bag in v:
+                n, bag = bag.split(' ', 1)
+                vd[bag] = int(n)
+        d[k] = vd
+
+    def count_bags(bag):
+        sub_bags = d[bag] 
+        if not sub_bags:
+            return 0
+        else:
+            return sum(sub_bags[b] * (1 + count_bags(b)) for b in sub_bags)
+
+    print(count_bags('shiny gold'))
 
 
-print(sum(can_carry_shiny(bag) for bag in d))
+if __name__ == "__main__":
+    part1()
+    part2()
