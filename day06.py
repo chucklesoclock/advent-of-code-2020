acda380d8b9e0c3f4d67f4d2b070d85d0efdb46d
Charlie G <charlieg895@gmail.com>
2020-12-08 19:55:18 -0600

    refactor and strings are iterables for sets duh

--- day06.py
+++ day06.py
@@ -1,17 +1,22 @@
+import numpy as np
+
 with open('input/data06.txt') as f:
     data = f.read().split('\n\n')
 
 
-def group_yes_count(group):
-    yes_answers = set()
-    for person in group.split('\n'):
-        yes_answers.update({ch for ch in person})
-    return len(yes_answers)
-
-
-def part1():
-    return sum(group_yes_count(group) for group in data)
+def group_yes_counts(group):
+    group = group.split('\n')
+    any_yes = set(group[0])
+    all_yes = any_yes.copy()
+    for person in group[1:]:
+        answer_set = set(person)
+        any_yes.update(answer_set)
+        all_yes.intersection_update(answer_set)
+    return len(any_yes), len(all_yes)
 
 
 if __name__ == "__main__":
-    print(part1())
+    any_yes_sum, all_yes_sum = np.sum(
+        [group_yes_counts(group) for group in data], axis=0)
+    print('any =', any_yes_sum)
+    print('all =', all_yes_sum)
