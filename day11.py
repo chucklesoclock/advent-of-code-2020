4ca98667bfd9bb01910cbcb427c9319f07e80284
Charlie Garavaglia <charlieg895@gmail.com>
2021-03-03 21:52:50 -0600

    day11 part 1 complete; refactor incoming, later

--- day11.py
+++ day11.py
@@ -87,19 +87,9 @@ class Seat:
         self.will_change = None
 
 if __name__ == "__main__":
-    sample_layout = """L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL"""
-    ferry = Ferry(sample_layout)
-    num_occupied = ferry.start_chaos().count_occupied()
-    print(num_occupied)
+    with open('input/data11.txt') as f:
+        layout = f.read()
+    print(Ferry(layout).start_chaos().count_occupied())
 
 def test_part_1_sample():
     sample_layout = """L.LL.LL.LL
