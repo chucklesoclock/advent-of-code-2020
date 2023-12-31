b8d8eaa26d0664a86f1c17553d950a7cdecce6f8
Charlie Garavaglia <charlieg895@gmail.com>
2023-12-31 11:36:02 -0600

    day 12 cheesey part 2 solution
    wanted to continue sin/cos route

---
+++ day12.py
@@ -0,0 +1,95 @@
+import pytest
+import math
+
+
+def parse(data: str):
+    return [(instruction[0], int(instruction[1:])) for instruction in data.splitlines()]
+
+
+def part1(data):
+    direction = 0
+    position = [0, 0]
+    for action, num in data:
+        match action:
+            case "N":
+                position[1] += num
+            case "S":
+                position[1] -= num
+            case "E":
+                position[0] += num
+            case "W":
+                position[0] -= num
+            case "R":
+                direction -= num
+                direction = direction % 360
+            case "L":
+                direction += num
+                direction = direction % 360
+            case "F":
+                position[0] += int(math.cos(math.radians(direction)) * num)
+                position[1] += int(math.sin(math.radians(direction)) * num)
+    return sum(abs(d) for d in position)
+
+
+def part2(data):
+    waypoint = [10, 1]
+    position = [0, 0]
+    for action, num in data:
+        match action:
+            case "N":
+                waypoint[1] += num
+            case "S":
+                waypoint[1] -= num
+            case "E":
+                waypoint[0] += num
+            case "W":
+                waypoint[0] -= num
+            case "R":
+                # rotate waypoint around ship clockwise
+                match num:
+                    case 90:
+                        waypoint[0], waypoint[1] = waypoint[1], -waypoint[0]
+                    case 180:
+                        waypoint[0], waypoint[1] = -waypoint[0], -waypoint[1]
+                    case 270:
+                        waypoint[0], waypoint[1] = -waypoint[1], waypoint[0]
+            case "L":
+                # rotate waypoint around ship counterclockwise
+                match num:
+                    case 90:
+                        waypoint[0], waypoint[1] = -waypoint[1], waypoint[0]
+                    case 180:
+                        waypoint[0], waypoint[1] = -waypoint[0], -waypoint[1]
+                    case 270:
+                        waypoint[0], waypoint[1] = waypoint[1], -waypoint[0]
+            case "F":
+                position[0] += waypoint[0] * num
+                position[1] += waypoint[1] * num
+
+    return sum(abs(d) for d in position)
+
+
+if __name__ == "__main__":
+    with open("input/data12.txt") as f:
+        data = parse(f.read())
+    print(part1(data))
+    print(part2(data))
+
+
+@pytest.fixture
+def sample_data():
+    return parse(
+        """F10
+N3
+F7
+R90
+F11"""
+    )
+
+
+def test_part_1(sample_data):
+    assert part1(sample_data) == 25
+
+
+def test_part_2(sample_data):
+    assert part2(sample_data) == 286
